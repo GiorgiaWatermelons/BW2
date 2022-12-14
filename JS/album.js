94ee88447132dc462acc85c76c2361f982d1a1c2
LorenzoCoratza <lorenzocoratza@gmail.com>
2022-12-14 18:08:09 +0100

    layout desktop quasi finito

--- JS/album.js
+++ JS/album.js
@@ -16,8 +16,14 @@ fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${search}`)
                                                    <div class="col-sm-2 col-md-2 col-lg-1" onclick="history.back()"><i class="bi bi-arrow-left freccia_indietro_album" "></i></div>
                                                    <img src=${response.cover} alt="img_album" class="col-sm-8 col-md-8 col-lg-4">
                                                    <div class="d-none d-lg-block col-lg-6 d-flex flex-column">
+                                                    <div class="col-12 h-25">
+                                                    </div>
                                                     <p class="d-none d-lg-block col-12">ALBUM</p>
-                                                   <h1 class="d-none d-lg-block col-12">${response.title}</h1>
+                                                    <h1 class="d-none d-lg-block col-12">${response.title}</h1>
+                                                    <div class="d-none d-lg-block col-12 d-flex flex-row">
+                                                       <img src="${response.artist.picture_small}" class="rounded-circle" alt="img_artista">
+                                                       <span>${response.artist.name} • ${response.release_date} • ${response.nb_tracks}, ${response.duration} </span>
+                                                      </div>
                                                    ///IMG-NOME ARTISTI-DATA-NUM.BRANI///
                                                     </div>
                                                    </div>
@@ -33,6 +39,7 @@ fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${search}`)
         loadAlbumArray(tracksArray); //carica albumArray
 
         impaginaAlto(response);
+       
         impaginaAlbum(); //impagina le tracks di albumArray nel divDegliAlbum
 
         gestisciCanzoneSelezionata(response); //gestisce il comportamento della scritta della canzone selezionata
